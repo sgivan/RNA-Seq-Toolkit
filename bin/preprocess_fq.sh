500f704b54c19d162739d2e6ab39fcc4645f8ce7
Scott A. Givan <givans@missouri.edu>
2010-12-08 17:41:21 -0600

    Added filter clf and associated code to set/change value. Added echo statements to output commands to STDOUT.

--- bin/preprocess_fq.sh
+++ bin/preprocess_fq.sh
@@ -11,6 +11,7 @@ min_length=32
 percent_high_quality=90
 bowtie_threads=8
 BOWTIE_INDEXES='index'
+filter='filter'
 #
 function help_messg {
     echo "invoke script like this:"
@@ -24,7 +25,7 @@ function help_messg {
 
 # command line option parsing adpated from /usr/share/doc/util-linux-2.13/getopt-parse.bash
 #
-TEMP=`getopt -o q:l:p:t:hi: --long min_qual:,min_length:,percent_high_quality:,bowtie_threads:,indexpath: -- "$@"`
+TEMP=`getopt -o q:l:p:t:hi:f: --long min_qual:,min_length:,percent_high_quality:,bowtie_threads:,indexpath:,filter: -- "$@"`
 
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 
@@ -38,6 +39,7 @@ while true ; do
         -p|--percent_high_quality) percent_high_quality=$2 ; shift 2 ;; 
         -t|--bowtie_threads) bowtie_threads=$2 ; shift 2 ;;
         -i|--indexpath) BOWTIE_INDEXES=$2 ; shift 2 ;;
+        -f|--filter) filter=$2 ; shift 2 ;;
         -h|help) help_messg ; exit ;;
 #        --) shift ; break ;;
         *) break ;;
@@ -55,18 +57,22 @@ cd preprocess
 ln -sf ../set1_input.fq ./set1.fq
 ln -sf ../set2_input.fq ./set2.fq
 echo "quality trimming and filtering"
-fastq_quality_trimmer -i set1.fq -t $min_qual -l $min_length -v 2> set1_qt.log | fastq_quality_filter -p $percent_high_quality -q $min_qual -o set1_qt_qf.fq -v > set1_qt_qf.log 
-fastq_quality_trimmer -i set2.fq -t $min_qual -l $min_length -v 2> set2_qt.log | fastq_quality_filter -p $percent_high_quality -q $min_qual -o set2_qt_qf.fq -v > set2_qt_qf.log
+echo "fastq_quality_trimmer -i set1.fq -t $min_qual -l $min_length -v 2> set1_qt.log | fastq_quality_filter -p $percent_high_quality -q $min_qual -o set1_qt_qf.fq -v | tee set1_qt_qf.log" 
+fastq_quality_trimmer -i set1.fq -t $min_qual -l $min_length -v 2> set1_qt.log | fastq_quality_filter -p $percent_high_quality -q $min_qual -o set1_qt_qf.fq -v | tee set1_qt_qf.log 
+echo "fastq_quality_trimmer -i set2.fq -t $min_qual -l $min_length -v 2> set2_qt.log | fastq_quality_filter -p $percent_high_quality -q $min_qual -o set2_qt_qf.fq -v | tee set2_qt_qf.log"
+fastq_quality_trimmer -i set2.fq -t $min_qual -l $min_length -v 2> set2_qt.log | fastq_quality_filter -p $percent_high_quality -q $min_qual -o set2_qt_qf.fq -v | tee set2_qt_qf.log
 echo "sequence similarity filtering using bowtie"
 echo "reads that fail to align are retained, reads that align are filtered out of data"
 echo "first data file ..."
-bowtie -q --solexa1.3-quals --un set1_qt_qf_sf.fq --threads $bowtie_threads filter set1_qt_qf.fq > set1_qt_qf_filter_matched.sam 2> set1_qt_qf_bwt.log
+echo "bowtie -q --solexa1.3-quals --un set1_qt_qf_sf.fq --threads $bowtie_threads $filter set1_qt_qf.fq > set1_qt_qf_filter_matched.sam 2> set1_qt_qf_bwt.log"
+bowtie -q --solexa1.3-quals --un set1_qt_qf_sf.fq --threads $bowtie_threads $filter set1_qt_qf.fq > set1_qt_qf_filter_matched.sam 2> set1_qt_qf_bwt.log
 cat set1_qt_qf_bwt.log
 
 if [[ -e set2_qt_qf.fq ]]
 then
 echo "second data file"
-bowtie -q --solexa1.3-quals --un set2_qt_qf_sf.fq --threads $bowtie_threads filter set2_qt_qf.fq > set2_qt_qf_filter_matched.sam 2> set2_qt_qf_bwt.log
+echo "bowtie -q --solexa1.3-quals --un set2_qt_qf_sf.fq --threads $bowtie_threads $filter set2_qt_qf.fq > set2_qt_qf_filter_matched.sam 2> set2_qt_qf_bwt.log"
+bowtie -q --solexa1.3-quals --un set2_qt_qf_sf.fq --threads $bowtie_threads $filter set2_qt_qf.fq > set2_qt_qf_filter_matched.sam 2> set2_qt_qf_bwt.log
 cat set2_qt_qf_bwt.log
 fi
 
