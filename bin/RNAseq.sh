c6f50fb8d972d53f2688e6dd9d2ad3685e2853a5
Scott A. Givan <givans@missouri.edu>
2010-12-10 07:21:10 -0600

    Lots of changes resulting from using test data and getting everything to work. Still need to work on PATH solution.

--- bin/RNAseq.sh
+++ bin/RNAseq.sh
@@ -7,7 +7,7 @@
 #
 # set path to find RNAseq scripts
 
-export PATH="~givans/projects/RNAseq/bin:~givans/bin:$PATH"
+export PATH="$HOME/ircf/sgivan/projects/RNAseq/bin:~/bin:$PATH"
 
 #
 # some variables to set ...
@@ -17,6 +17,9 @@ export PATH="~givans/projects/RNAseq/bin:~givans/bin:$PATH"
 tophat=tophat # because it's in my $PATH
 #cufflinks=/home/cgrb/givans/bin/cufflinks # full path to cufflinks
 cufflinks=cufflinks 
+
+bioclass=`pwd | sed -r 's/.+\/(.+)\/(.+)/\1/'`
+lane=`pwd | sed -r 's/.+\/(.+)\/(.+)/\2/' | sed 's/_//'`
 
 #
 # librarytype contains the "library type", as defined in the tophat documentation:
@@ -53,7 +56,8 @@ BOWTIE_INDEXES='index'
 
 seonly=0 # for single-end sequence data only -- no paired-end 
 procs=8 # number of processors to use
-run_type='partial'
+#run_type='partial'
+run_type='NULL'
 mate_inner_distance_r=165
 min_intron_length_i=50
 max_intron_length_I=25000
@@ -63,7 +67,7 @@ adapter_seq='NULL'
 # command line option parsing adpated from /usr/share/doc/util-linux-2.13/getopt-parse.bash
 #
 
-TEMP=`getopt -o pafhr:i:I:P:l:sA: --long full,aggregate,partial,mate_inner_distance:,min_intron_length:,max_intron_length:,procs:,librarytype:,indexpath:,refseq:,seonly,adapter_seq: -- "$@"`
+TEMP=`getopt -o pafhr:i:I:P:l:sA: --long full,transcripts,partial,mate_inner_distance:,min_intron_length:,max_intron_length:,procs:,librarytype:,indexpath:,refseq:,seonly,adapter_seq: -- "$@"`
 
 if [ $? != 0 ] ; then echo "Terminating..." ; exit 1 ; fi
 
@@ -74,7 +78,7 @@ while true ; do
     case "$1" in
         -f|--full) run_type='full' ; shift ;;
         -p|--partial) run_type='partial' ; shift ;;
-        -a|--aggregate) run_type='aggregate' ; shift ;;
+        -a|--transcripts) run_type='transcripts' ; shift ;;
         -r|--mate_inner_distance) mate_inner_distance_r=$2 ; shift 2 ;;
         -i|--min_intron_length) min_intron_length_i=$2 ; shift 2 ;;
         -I|--max_intron_length) max_intron_length_I=$2 ; shift 2 ;;
@@ -89,7 +93,9 @@ while true ; do
         *) break ;;
     esac
 done
-#echo "extra arguments: $@"
+echo "extra arguments: $@"
+echo "run type is " $run_type
+#exit
 
 #
 # tophatcmd, pe_extra_cmd and singles_extra_cmd contain the actual invocation of
@@ -102,7 +108,8 @@ pe_extra_cmd="-r $mate_inner_distance_r -o pe_tophat_out $fasta_file read_1 read
 singles_extra_cmd="-o singles_tophat_out $fasta_file read_1.1,read_2.1 "
 # 
 #cufflinkscmd="$cufflinks -m $mate_inner_distance_r -I $max_intron_length_I "
-cufflinkscmd="$cufflinks -I $max_intron_length_I --library-type $librarytype -r ../../index/refseq -p $procs"
+#cufflinksflgs="-I $max_intron_length_I --library-type $librarytype -r ../index/$fasta_file.fa -p $procs -o cufflinks -L $bioclass$lane --min-intron-length $min_intron_length_i */accepted_hits.bam"
+cufflinksflgs="-I $max_intron_length_I --library-type $librarytype -r ../index/$fasta_file.fa -p $procs -o cufflinks -L $bioclass$lane --min-intron-length $min_intron_length_i"
 
 #
 # END OF USER-DEFINED VARIABLES
@@ -153,10 +160,10 @@ then
         fi 
         ln -sf set1.fq read_1.1
     fi
-fi
+fi # end of preprocessing
 
 export BOWTIE_INDEXES # this is the directory containing the index files created with bowtie-build
-if [ $run_type = aggregate ]
+if [ $run_type = transcripts ]
 then
     if [[ $seonly -eq 0 ]]
     then
@@ -172,64 +179,71 @@ then
     fi
 
 else # maybe this should be a separate if clause
-    echo "running tophat without precomputed annotations"
-    if [[ $seonly -eq 0 ]]
+
+    if [[ $run_type != 'NULL' ]]
     then
-        echo $tophatcmd $pe_extra_cmd $@
-        $tophatcmd $pe_extra_cmd $@ > pe_tophat.stdout 2>&1
-        echo $tophatcmd $singles_extra_cmd $@
-        $tophatcmd $singles_extra_cmd $@ > singles_tophat.stdout 2>&1
-    else
-        singles_extra_cmd=`echo $singles_extra_cmd | sed 's/,read_2.1//'`
-        echo $tophatcmd $singles_extra_cmd $@
-        $tophatcmd $singles_extra_cmd $@ > singles_tophat.stdout 2>&1
+        echo "running tophat without precomputed annotations"
+        if [[ $seonly -eq 0 ]]
+        then
+            echo $tophatcmd $pe_extra_cmd $@
+            $tophatcmd $pe_extra_cmd $@ > pe_tophat.stdout 2>&1
+            echo $tophatcmd $singles_extra_cmd $@
+            $tophatcmd $singles_extra_cmd $@ > singles_tophat.stdout 2>&1
+        else
+            singles_extra_cmd=`echo $singles_extra_cmd | sed 's/,read_2.1//'`
+            echo $tophatcmd $singles_extra_cmd $@
+            $tophatcmd $singles_extra_cmd $@ > singles_tophat.stdout 2>&1
+        fi
+            
     fi
-        
 fi
 
-if [[ $run_type -eq full ]]
+if [[ $run_type = full ]]
 then
 
-    mkdir -p merged
-    cd merged
+#    mkdir -p merged
+#    cd merged
 
     if [[ $seonly -eq 0 ]]
     then
-        echo "now merging PE and SE alignment data"
+#        echo "now merging PE and SE alignment data"
+        echo "using cufflinks to build gene models with PE and SE alignment data"
         
         #echo "creating aggregate_junctions.txt"
         #cat ../*/junctions.bed | awk '{ if ($1 != "track") {split($11,len,","); split($12,blstrt,","); printf "%s\t%i\t%i\t%s\n", $1, $2 + len[1] - 1, $2 + blstrt[2], $6; }}' | sort -k 1,1 -gk 2,2 | uniq > aggregate_junctions.txt
-        echo "merging tophat bam files"
-        samtools merge merged.bam ../*/accepted_hits.bam
+#        echo "merging tophat bam files"
+#        samtools merge merged.bam ../*/accepted_hits.bam
+        echo $cufflinks $cufflinksflgs */accepted_hits.bam
+        $cufflinks $cufflinksflgs */accepted_hits.bam
     else
-        ln -fs ../singles_tophat_out/accepted_hits.bam ./merged.bam
+        echo "using cufflinks to build gene models with SE alignment data"
+#        ln -fs ../singles_tophat_out/accepted_hits.bam ./merged.bam
+        echo $cufflinks $cufflinksflgs */accepted_hits.bam
+        $cufflinks $cufflinksflgs */accepted_hits.bam
     fi
-    cd ..
+#    cd ..
 fi
 
-if [ $run_type = aggregate ]
+if [ $run_type = transcripts ]
 then
-    echo "running cufflinks using aggregate transcript file"
+    echo "running cufflinks using transcript file"
     #samtools merge - ../*/accepted_hits.bam | samtools view -o - - | cuff_sam_to_gff.pl --infile - --outfile all_sorted.gff --source GA2 --type $bioclass$lane
     
-    bioclass=`pwd | sed -r 's/.+\/(.+)\/(.+)/\1/'`
-    lane=`pwd | sed -r 's/.+\/(.+)\/(.+)/\2/' | sed 's/_//'`
-    
     echo "creating new directory: merged_aggregate"
-    mkdir -p merged_aggregate
-    cd merged_aggregate
-    echo "creating bam file from pe and single runs"
-    if [[ $seonly -eq 0 ]]
-    then
-        samtools merge merged.bam ../*/accepted_hits.bam 
-    else
-        ln -s ../singles_tophat_out/accepted_hits.bam ./merged.bam
-    fi
+#    mkdir -p merged_aggregate
+#    cd merged_aggregate
+#    echo "creating bam file from pe and single runs"
+#    if [[ $seonly -eq 0 ]]
+#    then
+#        samtools merge merged.bam ../*/accepted_hits.bam 
+#    else
+#        ln -s ../singles_tophat_out/accepted_hits.bam ./merged.bam
+#    fi
     echo "running cufflinks"
-    #cufflinks_extra_cmd="--GTF ../transcripts.gtf -L $bioclass$lane merged.bam > cufflinks.stdout 2>&1"
-    cufflinks_extra_cmd="--GTF ../transcripts.gtf -L $bioclass$lane merged.bam"
-    echo "$cufflinks $cufflinks_extra_cmd"
-    $cufflinks $cufflinks_extra_cmd
+    #cufflinks_extra_cmd="--GTF ../transcripts.gtf -L $bioclass$lane merged.bam"
+    cufflinks_extra_cmd="--GTF ../transcripts.gtf"
+    echo $cufflinks $cufflinksflgs $cufflinks_extra_cmd */accepted_hits.bam
+    $cufflinks $cufflinksflgs $cufflinks_extra_cmd */accepted_hits.bam
     #cd ..
     #ln -s merged/aggregate_junctions.txt junctions.txt
     #ln -s merged/transcripts.gtf ./
@@ -237,3 +251,23 @@ then
     #echo "rerunning tophat with merged data"
     
 fi
+
+mkdir -p merged
+cd merged
+
+if [[ $seonly -eq 0 ]]
+then
+
+    echo "merging PE and SE bam files"
+    samtools merge merged.bam ../*/accepted_hits.bam
+
+else
+
+    ln -s ../singles_tophat_out/accepted_hits.bam ./merged.bam
+fi
+
+cd ..
+
+echo "finished"
+echo ""
+
