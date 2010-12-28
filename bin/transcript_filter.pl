704ad343a979fa255b3fa26dd048a02a0e4dc806
Scott Givan <sgivan@yahoo.com>
2010-12-28 14:30:27 -0800

    Perl script to filter GTF file by length of feature.
    Former-commit-id: 265b3872fe8ee31595ca05cd4d594895ca56726b
    Former-commit-id: abc6cf56f64747a15546b154e59b3f5dbc6a8b69

---
+++ bin/transcript_filter.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+#
+use strict;
+use warnings;
+use Getopt::Long;
+
+my ($debug,$infile,$min_length,$max_length,$outfails,$failsfile,$cumulative);
+
+GetOptions(
+
+    "file=s"            =>  \$infile,
+    "min_length=i"      =>  \$min_length,
+    "max_length=i"      =>  \$max_length,
+    "outfails"          =>  \$outfails,
+    "cumulative"        =>  \$cumulative,
+    "debug"             =>  \$debug,
+);
+
+$infile = "-" unless ($infile);
+$min_length ||= 500;
+$max_length ||= 1e6;
+
+open(IN,$infile) or die "can't open $infile: $!";
+if ($outfails) {
+    $failsfile = 'outfails.gtf';
+    open(FAILS,">$failsfile") or die "can't open 'outfails.txt': $!";
+}
+
+my ($new_transcript_id,$transcript_id,$start,$stop,@buffer,$cnt,$cumlength);
+($start,$stop,$cnt,$cumlength) = (0,0,0,0);
+while (<IN>) {
+    my $line = $_;
+    my @vals = split /\t/, $line;
+    if ($vals[8] =~ /\Wtranscript_id\s\"(.+?)\"\;/) {
+        $new_transcript_id = $1;
+    }
+    $transcript_id = $new_transcript_id if (!$cnt);
+    
+    if ($new_transcript_id ne $transcript_id) {
+#        print @buffer; 
+        evalout($start,$stop,$cumlength,@buffer);
+        @buffer = ();
+        $transcript_id = $new_transcript_id;
+        ($start,$stop,$cumlength) = ($vals[3],$vals[4],0);
+    }
+
+    push(@buffer,$line);
+    $start = $vals[3] if ($vals[3] < $start || !$start);
+    $stop = $vals[4] if ($vals[4] > $stop);
+    $cumlength += $vals[4] - $vals[3] + 1;
+#    print "loop '$cnt', start: '$start', vals[3]: '" . $vals[3] . "', stop: '$stop', vals[4]: '" . $vals[4] . "'\n";
+
+
+} continue {
+    ++$cnt;
+    if (eof) {
+#        print @buffer;
+        evalout($start,$stop,$cumlength,@buffer);
+    }
+}
+
+close(IN) or warn("can't close $infile properly: $!");
+close(FAILS) or warn("can't close $failsfile properl: $!") if ($outfails);
+
+sub evalout {
+    my ($start,$stop,$cumlength,@bufferout) = @_;
+    my $diff = $stop - $start;
+
+#    if ($diff >= $min_length && $diff <= $max_length) {
+    if (($cumulative && ($cumlength >= $min_length && $cumlength <= $max_length)) || (!$cumulative && ($diff >= $min_length && $diff <= $max_length))) {
+        print "\nstart: $start; stop: $stop; cumulative: $cumlength\n" if ($debug);
+        print @bufferout;
+    } elsif ($outfails) {
+        print FAILS @bufferout;
+    }
+}
