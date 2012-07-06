ee2c90a8fa314a2d1edf291fb9f473c36c9a11d7
Scott A. Givan <sgivan@yahoo.com>
2012-07-06 15:12:51 -0500

    Added GPL text to each file.
    Former-commit-id: 8a0195e2ca386ff07736fdf892cdd3307b3682e2
    Former-commit-id: 0314d47010c0602bafeb30317cfb8a83df87b330

--- bin/RNAseq_process_data.sh
+++ bin/RNAseq_process_data.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 #
+# copyright Scott Givan, The University of Missouri, July 6, 2012
+#
+#    This file is part of the RNA-seq Toolkit, or RST.
+#
+#    RST is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RST is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RST.  If not, see <http://www.gnu.org/licenses/>.
+#
 # run this script from directory containing flowcell directories; ie, the one containing all the FCXXX directories
 # then, invoke like FGMG_process_data.sh FC???/s_? 
 wd=`pwd`
