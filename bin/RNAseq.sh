8a0195e2ca386ff07736fdf892cdd3307b3682e2
Scott A. Givan <sgivan@yahoo.com>
2012-07-06 15:12:51 -0500

    Added GPL text to each file.

--- bin/RNAseq.sh
+++ bin/RNAseq.sh
@@ -3,7 +3,23 @@
 # This bash script runs the program "tophat" on a series of fastq
 # files of short read sequences
 #
-# copyright Scott Givan, The University of Missouri, November 11, 2011.
+# copyright Scott Givan, The University of Missouri, July 6, 2012
+#
+#    This file is part of the RNA-seq Toolkit, or RST.
+#
+#    RST is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RST is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RST.  If not, see <http://www.gnu.org/licenses/>.
+#
 #
 # set path to find RNAseq scripts
 wd=`pwd`
