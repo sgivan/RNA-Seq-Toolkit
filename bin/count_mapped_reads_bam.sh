f947c569687c844033feff241ce25a18ba54752c
Scott A. Givan <sgivan@yahoo.com>
2013-10-10 09:14:21 -0500

    Add line to use gnu parallel sort and /scratch as a temporary directory on IRCF BioCluster.
    Former-commit-id: dc22836f4473a56af54623a4b3df184336da71ac
    Former-commit-id: 18d12ec42ccf1d9e9230c96bec7630c615c70113

--- bin/count_mapped_reads_bam.sh
+++ bin/count_mapped_reads_bam.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-samtools view merged.bam | cut -f 1 | psort --parallel=4 | uniq | wc -l > merged.cnt
+file=$1
+#samtools view merged.bam | cut -f 1 | psort --parallel=4 --temporary-directory=/scratch | uniq | wc -l > merged.cnt
+#samtools view $file | cut -f 1 | psort --parallel=4 --temporary-directory=/scratch | uniq | wc -l > ${file}.cnt
+# use following line on BioCluster
+#samtools view $file | grep -v '^@' | awk '{ if ($2 != 4) { print $0; } }' | cut -f 1 | psort --parallel=4 --temporary-directory=/scratch | uniq | wc -l > ${file}.cnt
+# for most machines, don't use psort and /scratch, b/c they may not be available
+# psort is just a link to a newer version of gnu sort that is threaded
+# https://www.gnu.org/software/coreutils/manual/html_node/sort-invocation.html
+#
+samtools view $file | grep -v '^@' | awk '{ if ($2 != 4) { print $0; } }' | cut -f 1 | sort | uniq | wc -l > ${file}.cnt
