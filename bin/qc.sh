b703c040046fbcd1309f354403f971237ab0ddd7
Scott A. Givan <sgivan@yahoo.com>
2013-10-10 09:19:14 -0500

    Added --quiet option to not generate output to STDOUT.
    Former-commit-id: 560273bb8d374ff119331b6d29e8da319776c0e8
    Former-commit-id: cd5830982a6341a6562593ce2768c9432eccaa54

--- bin/qc.sh
+++ bin/qc.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
+TEMP=`getopt -o q -- "$@"`
+
+quiet=0
+
+eval set -- "$TEMP"
+while true ; do
+    case "$1" in
+        -q) quiet=1 ; shift ;;
+        --) shift ; break ;;
+        *) break ;;
+    esac
+done
+
 for dir
 do
-    echo $dir
+#    echo $dir
     cd $dir
-#    total=`fqseqs set1_00.fq`
-#    echo "raw seqs $total"
     cd preprocess
-#    cat *.log
     raw=`grep Input set1_qt.log | cut -f 2 -d " "`
     qt=`grep discarded set1_qt.log | awk '{ print $2; }'`
     qf=`grep discarded set1_qt_qf.log | awk '{ print $2; }'`
-#    qm=`grep Reported set1_qt_qf_bwt.log | cut -f 2 -d " "`# doesn't work with bowtie2
     qct=`grep 'aligned 0 times' set1_qt_qf_bwt.log | awk '{ print $1; }'`
     qcfi=`grep reads set1_qt_qf_bwt.log | cut -f 1 -d " "`
     qm=`expr $qcfi - $qct`
     rtn=`echo "scale=4; ($qct/$raw)*100" | bc | sed 's/00\$//'`
-    echo "trimmed: $qt"
-    echo "filtered: $qf"
-    echo "matched: $qm"
-    echo "qc reads: $qct"
-    echo "% retained: $rtn"
+    if [[ $quiet -eq 0 ]]
+    then
+        echo $dir
+        echo "trimmed: $qt"
+        echo "filtered: $qf"
+        echo "matched: $qm"
+        echo "qc reads: $qct"
+        echo "% retained: $rtn"
+    fi
     cd ../../
     echo "|$dir|$raw|$qt|$qf|$qm|$qct|$rtn|"
-    echo
+#    echo
 done
 
 
