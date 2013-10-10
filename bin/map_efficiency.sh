39c7b148dee2db1e06b50eaf882d2332ee668982
Scott A. Givan <sgivan@yahoo.com>
2013-10-10 09:17:33 -0500

    Added -q option to no generate as much output on STDOUT.

--- bin/map_efficiency.sh
+++ bin/map_efficiency.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
+TEMP=`getopt -o q -- "$@"`
+
+quiet=0
+
+eval set -- "$TEMP"
+while true ; do
+    case "$1" in
+        -q) quiet=1 ; shift ;;
+        --) shift ; break ;;
+        *) break ;;
+    esac
+done
+
 for dir
 do
-    echo $dir
+#    echo $dir
     cd $dir
     cd preprocess
     qct=`grep 'aligned 0 times' set1_qt_qf_bwt.log | awk '{ print $1; }'`
     cd ../merged
-    mapped=`cat merged.cnt`
+#    mapped=`cat merged.cnt`
+    mapped=`cat merged.bam.cnt`
     rtn=`echo "scale=4; ($mapped/$qct)*100" | bc | sed 's/00\$//'`
-    echo "qc reads: $qct"
-    echo "mapped reads: $mapped"
+    if [[ $quiet -eq 0 ]]
+    then
+        echo $dir
+        echo "qc reads: $qct"
+        echo "mapped reads: $mapped"
+    fi
     cd ../../
-    echo "|$qct|$mapped|$rtn|"
+    echo "|$dir|$qct|$mapped|$rtn|"
 done
 
 
