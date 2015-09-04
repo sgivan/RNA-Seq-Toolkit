2d325ad0c5cf8a7317ccfa29ba4d704575ab3a46
Scott A. Givan <sgivan@yahoo.com>
2015-09-04 13:04:46 -0500

    Getting the fastqc functionality to work
    Former-commit-id: 6eb92b3e18174194ccdf8aa34629e0651adaaa8e
    Former-commit-id: db9ace09fb3ae5814a5d368f06d2eecca5c2f8e2

--- bin/fastqc_loop.sh
+++ bin/fastqc_loop.sh
@@ -5,10 +5,11 @@ do
     echo $dir
     cd $dir
 #    mkdir -p fastqc
-#    $( bsub -J $dir -R 'span[hosts=1]' -n 4 fastqc --threads 4 --outdir fastqc set1.fq )
+#    $( bsub -o fastqc.o -e fastqc.e -J ${dir} -R 'span[hosts=1]' -n 4 "fastqc --threads 4 --outdir fastqc set1.fq; cd fastqc; mv set1_fastqc.html ${dir}_set1_fastqc.html; cd ..;" )
+#    mv set1_fastqc.html ${dir}_set1_fastqc.html
 
-    mv set1_fastqc.html ${dir}_set1_fastqc.html
+    $( sed "s/JOBNAME/${dir}/" ~/projects/RNA-Seq-Toolkit/bin/fastqc.bs > fastqc.bs )
+    bsub < fastqc.bs
     cd ..
 done
-
 
