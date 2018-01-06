b2c8e92ef3a229b398f43fe944a5cba0130155f6
Scott A. Givan <sgivan@yahoo.com>
2018-01-06 17:41:36 -0600

    Added "module load" statements to beginning of setup script.

--- test/TestData_3/setup_and_test.sh
+++ test/TestData_3/setup_and_test.sh
@@ -17,6 +17,11 @@
 #    You should have received a copy of the GNU General Public License
 #    along with RST.  If not, see <http://www.gnu.org/licenses/>.
 #
+module load bowtie2-2.3.2
+module load stringtie-1.3.0
+module load HISAT2-2.1.0
+module load Python-shared
+module load R-3.3.0-sharedlib
 
 echo "creating symbolic link to RNAseq tools directory"
 ln -s ../../bin ./
