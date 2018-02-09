2247e04ca5e4efef4bee94b2768c9de50a2545f7
Christopher Bottoms <molecules@cpan.org>
2018-02-09 16:22:51 -0600

    reset test before running it

--- test/TestData_4/t/setup_and_test_2.0.4.sh
+++ test/TestData_4/t/setup_and_test_2.0.4.sh
@@ -17,6 +17,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with RST.  If not, see <http://www.gnu.org/licenses/>.
 #
+./reset_test
 module load bowtie2-2.3.2
 module load stringtie-1.3.0
 t/test.t 2.0.4 $@
