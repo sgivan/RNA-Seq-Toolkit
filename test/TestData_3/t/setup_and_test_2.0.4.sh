0ce7289e81e16aed3f0f707d7faee7ed80ac1c75
Christopher Bottoms <molecules@cpan.org>
2018-02-09 14:45:33 -0600

    we have tests for both 2.0.4 and 2.1.0, but we default to using 2.0.4

--- test/TestData_3/t/setup_and_test_2.0.4.sh
+++ test/TestData_3/t/setup_and_test_2.0.4.sh
@@ -19,5 +19,5 @@
 #
 module load bowtie2-2.3.2
 module load stringtie-1.3.0
-module load HISAT2-2.1.0
-./setup_and_test.sh
+module load HISAT2-2.0.4
+t/versionless_setup_and_test.sh
--- test/TestData_3/t/setup_and_test_2.1.0.sh
+++ test/TestData_3/t/setup_and_test_2.1.0.sh
@@ -20,4 +20,4 @@
 module load bowtie2-2.3.2
 module load stringtie-1.3.0
 module load HISAT2-2.1.0
-./setup_and_test.sh
+t/versionless_setup_and_test.sh
