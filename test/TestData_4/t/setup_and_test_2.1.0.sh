6c0633b5b17e420b4b304578fb97553dd9439957
Christopher Bottoms <molecules@cpan.org>
2018-02-21 11:32:30 -0600

    uncoupled test files for 3 and 4

--- test/TestData_4/t/setup_and_test_2.1.0.sh
+++ test/TestData_4/t/setup_and_test_2.1.0.sh
@@ -1,1 +1,24 @@
-../../common_to_3_4/setup_and_test_2.1.0.sh
+#!/bin/bash
+#
+#   Copyright Scott A. Givan, University of Missouri, July 6, 2012.
+#
+#    This file is part of the RNA-seq Toolkit, or RST.
+#
+#    RST is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RST is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RST.  If not, see <http://www.gnu.org/licenses/>.
+#
+./reset_test
+export HISAT_VERSION_BEING_TESTED=2.1.0
+TESTDATA_TEST_LOG="test_${HISAT_VERSION_BEING_TESTED}.log";
+t/versionless_setup_and_test.sh |& tee $TESTDATA_TEST_LOG
+t/test.t $HISAT_VERSION_BEING_TESTED $@ |& tee --append $TESTDATA_TEST_LOG
