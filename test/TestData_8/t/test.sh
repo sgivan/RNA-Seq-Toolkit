1a592aeefaa43370a1d678a423e562fd17b7c6be
Christopher Bottoms <molecules@cpan.org>
2018-02-22 10:40:28 -0600

    making separate tests for running locally versus on open-lava

--- test/TestData_8/t/test.sh
+++ test/TestData_8/t/test.sh
@@ -5,11 +5,6 @@
 # Creates a merged transcript gtf file.
 # Then, re-runs HISAT2 pipepline using the merged GTF for all samples.
 
-module load Python-shared
-module load R-3.3.0-sharedlib
-module load bowtie2-2.3.2
-module load stringtie-1.3.0
-module load HISAT2-2.1.0
 
 echo "running using hisat2 version '`hisat2 --version | head -n1`'"
 
