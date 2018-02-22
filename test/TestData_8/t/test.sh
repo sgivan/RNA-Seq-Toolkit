af6d5ee0607a9831f9d8544651aebb1e79bf4b03
Christopher Bottoms <molecules@cpan.org>
2018-02-22 10:46:31 -0600

    give verbose directory listings only for DEBUG mode

--- test/TestData_8/t/test.sh
+++ test/TestData_8/t/test.sh
@@ -5,21 +5,18 @@
 # Creates a merged transcript gtf file.
 # Then, re-runs HISAT2 pipepline using the merged GTF for all samples.
 
+DEBUG=0
 
-echo "running using hisat2 version '`hisat2 --version | head -n1`'"
-
-# Sanity check (Are we where we want to be and have the files been copied?):
-sanity_check() {
-    echo ""
-    echo "---------------------------------------------------------"
-    echo "------------- Files in `pwd` and subfolders -------------"
-    ls *
-    echo "---------------------------------------------------------"
-    echo "---------------------------------------------------------"
-    echo ""
+# Sanity check (Are we where we want to be with the files we expect?)
+debug_check_pwd_and_files() {
+    if [ $DEBUG -eq 1 ]; then 
+        echo "------------- Files in `pwd` and subfolders -------------"
+        ls -A *
+        echo "---------------------------------------------------------"
+    fi
 }
 
-sanity_check
+debug_check_pwd_and_files
 
 #EXPLANATION OF FLAGS USED (listed alphabetically)
 # -a use "transcripts.gtf" for gene models and skip preprocessing
@@ -28,22 +25,22 @@ sanity_check
 # -H number of threads to use
 # -t generate gtf file of empirical transcripts
 # -Y Phred quality values encoded as Phred + 64 
-../../bin/RNAseq_process_data.sh -e -H 4 -t -f -Y s_?
+RNAseq_process_data.sh -e -H 4 -t -f -Y s_?
 
-sanity_check
+debug_check_pwd_and_files
 
-../../bin/RNAseq_process_data.sh -e -H 4 -a -Y  s_?
+RNAseq_process_data.sh -e -H 4 -a -Y  s_?
 
-sanity_check
+debug_check_pwd_and_files
 
 # run ballgown
 ../../bin/ballgown_setup.pl --cont "s_[56]" --exp "s_[78]"
 
-sanity_check
+debug_check_pwd_and_files
 
-../../R/ballgown.Rscript 
+ballgown.Rscript 
 
-sanity_check
+debug_check_pwd_and_files
 
 # Test result files
 t/test.t $HISAT_VERSION_BEING_TESTED
