a5c6656d46bbed8b0052a59b0c257e38a5ac4b21
Scott A. Givan <sgivan@yahoo.com>
2018-04-19 14:18:03 -0500

    Still fiddling with the R script. This is a working version.
    Former-commit-id: ef17f8e974f7f535319c2d5ce76418f2adc00411
    Former-commit-id: 9f1e12a7acd23b8189c7060e588a7965094863c4

--- templates/DESeq2.R
+++ templates/DESeq2.R
@@ -1,11 +1,11 @@
 #!/bin/env Rscript
-#SBATCH -J DESeq2
-#SBATCH -o out_DESeq2.o_%j
-#SBATCH -e out_DESeq2.e_%j
+#SBATCH -J $prefix
+#SBATCH -o ${prefix}_%J.o
+#SBATCH -e ${prefix}_%J.e 
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --mem=40G
+#SBATCH --mem=$memory
 
 library(edgeR)
 library(DESeq2)
@@ -17,26 +17,34 @@ library(dplyr)
 plot_Volcano <- function(DE_results_filename,labelPoints=FALSE) {
 
     res <- read.table(DE_results_filename, na.strings="", sep="\t",header=TRUE)
-    row.names(res) <- res$Gene
+    row.names(res) <- res$$Gene
 
     # No NA's in log2FoldChange, right?
     # True is also 1, right?
-    log2fc <- res$log2FoldChange
+    log2fc <- res$$log2FoldChange
     circle_sizes <- rep(1,length(log2fc))
 
     res <- res%>%mutate(color = ifelse(abs(log2FoldChange) >= 2,ifelse(padj<=0.05 , "green", "orange"),ifelse(padj<=0.05,"red","black")))
-    max_y <- max(-log10(res$pvalue)) + 0.5
+    max_y <- max(-log10(res$$pvalue)) + 0.5
 
     ggplot(res, aes(x=log2FoldChange, y=-log10(pvalue), ymax=max_y, ymin=0)) +
     geom_point(aes(colour=color), size=2.5) +
     scale_colour_manual(name="", values = c("green"="green", "orange"="orange", "red"="red", "black"="black")) +
 
     theme(axis.text=element_text(size=12), axis.title=element_text(size=16)) + # Make axis text and title bigger
-    guides(color=TRUE)
+    guides(color=FALSE)
 
 }
-
-data = read.table("gene_cnt_matrix.tab", header=TRUE, row.names=1, com='', na.strings="", sep="\t")
+#
+# The gene_cnt_matrix.tab file, loaded in the next command, should have gene IDs in the first column.
+# Then, each sample should occupy its own column.
+# Control coumns should be the columns after the first.
+# Experimental columns should follow after all the control columns.
+# the header should reflect the columns. For example:
+# GeneID    Sample_1    Sample_2    Sample_3    Sample_4
+# In this example, Sample_1 and Sample_2 are controls; Sample_3 and Sample_4 are the experimentals.
+data = read.delim("$datafile", header=T, row.names=1, sep=" ", stringsAsFactors=F)
+# may need round() for RSEM data. Not needed for STAR.
 #rnaseqMatrix = round(data)
 rnaseqMatrix = data
 
@@ -45,7 +53,9 @@ rnaseqMatrix = rnaseqMatrix[rowSums(cpm(rnaseqMatrix) > 1) >= 2,]
 
 # Create a dataframe of condition labels
 #condition = data.frame(condition=factor(c(rep("D", 5), rep("C",5))))
-condition = data.frame(condition=factor(c(rep("Cont", $cntCont), rep("Exp",$numExp))))
+# $cntCont and $cntExp will be replaced by the number of Control samples and the number
+# of Experimental samples, respectively.
+condition = data.frame(condition=factor(c(rep("Cont", $cntCont), rep("Exp",$cntExp))))
 sample_IDs <- colnames(rnaseqMatrix)
 
 # Add rownames that correspond to sample IDs
@@ -61,29 +71,26 @@ dds = DESeq(ddsFullCountTable)
 # plotPCA can't take dds straight
 rldds <- rlog(dds)
 
-pdf("D_vs_C__DESeq2PCA_rldds.pdf")
+pdf(paste0("$prefix", "_PCA.pdf"))
 plotPCA(rldds,intgroup=c('condition')) # This will give groups, but not sample names within the group
 dev.off()
 
-png("D_vs_C__DESeq2PCA_1200x1200.png",width=1200,height=1200)
+svg(paste0("$prefix", "_PCA.svg"))
 plotPCA(rldds,intgroup=c('condition')) # This will give groups, but not sample names within the group
 dev.off()
 
-png("D_vs_C__DESeq2PCA_rldds.png",width=480,height=480)
-plotPCA(rldds,intgroup=c('condition')) # This will give groups, but not sample names within the group
-dev.off()
-
-contrast=c("condition","D","C")
+#contrast=c("condition","D","C")
+contrast=c("condition","Exp","Cont")
 res = results(dds, contrast)
 
-res = cbind(experimental="D", control="C", as.data.frame(res))
+res = cbind(experimental="Exp", control="Cont", as.data.frame(res))
 
 # Add baseMean Exp and Control
-baseMeanExp     <- rowMeans(counts(dds, normalized=TRUE)[,colData(dds)$condition == "D"])
-baseMeanControl <- rowMeans(counts(dds, normalized=TRUE)[,colData(dds)$condition == "C"])
+baseMeanExp     <- rowMeans(counts(dds, normalized=TRUE)[,colData(dds)$$condition == "Exp"])
+baseMeanControl <- rowMeans(counts(dds, normalized=TRUE)[,colData(dds)$$condition == "Cont"])
 res = cbind(baseMeanExp, baseMeanControl, as.data.frame(res))
 
-res$padj[is.na(res$padj)]  <- 1
+res$$padj[is.na(res$$padj)]  <- 1
 geneSymbol <- mapIds(org.Mm.eg.db, keys=row.names(res), column="SYMBOL",keytype="ENSEMBL", multiVals="first")
 bestGeneDescriptor <- geneSymbol
 bestGeneDescriptor[ is.na(bestGeneDescriptor) ] <- row.names(res)[ is.na(bestGeneDescriptor) ]
@@ -92,18 +99,17 @@ res <- cbind(GeneSymbol=geneSymbol, as.data.frame(res))
 res <- cbind(Gene=row.names(res), as.data.frame(res))
 res <- cbind(BestGeneDescriptor=bestGeneDescriptor, as.data.frame(res))
 
-write.table(as.data.frame(res[order(res$pvalue),]), file='D_vs_C__DESeq2.DE_results', sep="\t", na="", row.names=FALSE,quote=FALSE)
-write.table(rnaseqMatrix, file='D_vs_C__out.count_matrix', sep="\t", na="", row.names=FALSE,quote=FALSE)
+#write.table(as.data.frame(res[order(res$$pvalue),]), file='DESeq2_DE_results.txt', sep="\t", na="", row.names=FALSE,quote=FALSE)
+write.table(as.data.frame(res[order(res$$pvalue),]), file=paste0("$prefix", "_DESeq2_DE_results.txt"), sep="\t", na="", row.names=FALSE,quote=FALSE)
+write.table(rnaseqMatrix, file=paste0("$prefix", "_DESeq2_count_matrix.txt"), sep="\t", na="", row.names=FALSE,quote=FALSE)
 
-pdf("D_vs_C__Volcano.pdf")
-plot_Volcano('D_vs_C__DESeq2.DE_results')
+pdf(paste0("$prefix", "_Volcano.pdf"))
+plot_Volcano(paste0("$prefix", "_DESeq2_DE_results.txt"))
 dev.off()
 
-png("D_vs_C__Volcano.png",width=480,height=480)
-plot_Volcano('D_vs_C__DESeq2.DE_results')
+png(paste0("$prefix", "_Volcano.svg"))
+plot_Volcano(paste0("$prefix", "_DESeq2_DE_results.txt"))
 dev.off()
 
-png("D_vs_C__Volcano_1200x1200.png",width=1200,height=1200)
-plot_Volcano('D_vs_C__DESeq2.DE_results')
-dev.off()
+save.image(file=paste0("$prefix", "_RData"))
 
