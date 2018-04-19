fce5161bfa7dd68a21e4328268da4dabd937ccd0
Scott A. Givan <sgivan@yahoo.com>
2018-04-19 14:18:41 -0500

    Added script to generate an Rscript file using a template.
    Former-commit-id: 64697efed459d3c5b67ccf12ef162d93e66bc88b
    Former-commit-id: 4e287fe783ec09a4215fc2f002004c6041449a39

---
+++ bin/create_DESeq2_cmd_sbatch_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os, sys
+import argparse
+from string import Template
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--memory", help="amount of ram; ie '25G'", type=str, default='50G')
+parser.add_argument("--template", help="name of template file", type=str, default="DESeq2.R")
+parser.add_argument("--numberOfControls", help="the number of control samples", type=str, default=3)
+parser.add_argument("--numberOfExperimentals", help="the number of experimental samples", type=str, default=3)
+parser.add_argument("--prefix", help="prefix for output files", type=str, default="output_")
+parser.add_argument("--datafile", help="name of data file containing gene count data", type=str, default="gene_cnt_matrix.tab")
+
+args = parser.parse_args()
+
+wd = os.path.dirname(os.path.realpath(__file__))
+
+template_file = open(wd + "/../templates/" + args.template, 'r').read()
+
+s = Template(template_file)
+
+new_t = s.substitute(memory=args.memory, cntCont=args.numberOfControls, cntExp=args.numberOfExperimentals, prefix=args.prefix, datafile=args.datafile)
+
+print new_t
