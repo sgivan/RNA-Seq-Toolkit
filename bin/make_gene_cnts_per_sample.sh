9af9768a4c37853197312f3e24453588d2ec5135
Scott A. Givan <sgivan@yahoo.com>
2018-06-27 16:28:34 -0500

    Instead of keeping the unstranded output from STAR, use the reverse strand by default.
    Former-commit-id: 0f42dc1d81543d48d48c472b354de78e76c3c407
    Former-commit-id: 156b2543d5635e3a1e78b8497f837946803e8b7b

--- bin/make_gene_cnts_per_sample.sh
+++ bin/make_gene_cnts_per_sample.sh
@@ -19,7 +19,7 @@ do
     cd $dir;
     $(echo -e "EMBLID\t${dir}" > gene_cnts.txt)
 
-    $(grep -v "N_" merged_ReadsPerGene.out.tab | cut -f 1,2 >> gene_cnts.txt)
+    $(grep -v "N_" merged_ReadsPerGene.out.tab | cut -f 1,4 >> gene_cnts.txt)
 
     cd ..
 
