7fe6f33dd4054f30af732f84119deaea000e1cd7
Scott A. Givan <sgivan@yahoo.com>
2018-10-12 08:29:17 -0500

    Still working. Now creates "current" directory.
    Former-commit-id: 5a6b2812fd9efb7650e2a2140cfc66dfac4a5925
    Former-commit-id: e91ab9600a2e2e23954e28a2045abb0ab2144fe7

--- bin/rst_datafiles_setup.py
+++ bin/rst_datafiles_setup.py
@@ -11,23 +11,55 @@ argparser.add_argument("--verbose", action="store_true", help="verbose messages 
 
 args = argparser.parse_args()
 
-print 'input file: %(filename)s' % { "filename": args.infile }
+if args.verbose: print 'input file: %(filename)s' % { "filename": args.infile }
 
 f_yaml = open(args.infile, 'r') # open infile
 
 config = yaml.load(f_yaml)
 
-print "dumping config"
+if args.verbose: print "dumping config"
 print yaml.dump(config)
 
-#print config['input']['control'][0]
-print "first experimental file of first experimental set: '%(e1)s'." % { "e1": config['input']['experimental'][0][0] }
+#
+# create new working directory
+# fail if the directory already exists
+#
+if not os.access(config['working_datadir'], os.F_OK):
+    if args.verbose: print "creating new directory to place renamed files: %(newdir)s." % { "newdir": config['working_datadir'] }
+    if os.access(os.path.split(config['working_datadir'])[0], os.W_OK):
+        os.mkdir(config['working_datadir'])
+    else:
+        print "can't create the directory"
+        sys.exit(2)
+else:
+    print "%(newdir)s already exists. Will not overwrite -- please rename or move the diretory." % { "newdir": config['working_datadir'] }
+    print "Exiting now."
+    sys.exit(3)
+
+#
+# end of working directory section
+#
 
 elength=len(config['input']['experimental'])
-print "experimental data sets: %(length)i" % { "length": elength }
+sample_number=0
+if args.verbose: print "experimental data sets: %(length)i" % { "length": elength }
 for i in range(0,elength):
-    print "experimental file set %(eset)i: %(filenames)s" % { "eset": i, "filenames": config['input']['experimental'][i] }
-#for fileset in config['input']['experimental']:
-#    print "file set: '%(setname)s'" % { "setname": fileset }
+#   This cut corresponds to the sample replicate. There can be any number of sample replicates. which will have either a single file (non-PE) or a pair of files (Paired End)
+    print "sample replicates in set %(eset)i: %(filenames)s" % { "eset": i, "filenames": config['input']['experimental'][i] }
+    number_of_reps=len(config['input']['experimental'][i])
+    print "number of replicates: %(numseqs)i." % { "numseqs": number_of_reps }
+    for j in range(0,number_of_reps):
+        number_of_seq_files=len(config['input']['experimental'][i][j])
+        print "number of sequences in %(seqfiles)s: %(numseqfiles)i" % { "seqfiles": config['input']['experimental'][i][j], "numseqfiles": number_of_seq_files }
+        if number_of_seq_files == 2:
+            if args.verbose: print "\tworking with paired-end data"
+        else:
+            if args.verbose: print "\tworking with non-paired-end data"
 
+        sample_number += 1
+        if args.verbose: print "\tThis replicate will be given symbolic name 'Sample_%(sint)s'" % { "sint": sample_number }
+
+
+print str(sample_number) + ' samples'
 print "OK"
+
