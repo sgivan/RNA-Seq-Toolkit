ba5938ae189e91b4bbb912f75abd35134dc25f9e
Scott Givan <sgivan@yahoo.com>
2019-06-14 10:38:15 -0400

    setup.sh now creates read_1 and read_2 symlinks
    Former-commit-id: d8341119e570f9ba973006363394f37e933f07bd
    Former-commit-id: 0270f042cd4c678235cd1bab31ab6c8757d1b3fa

--- bin/setup.sh
+++ bin/setup.sh
@@ -10,9 +10,11 @@ do
     file=$( ls -1 ../$dir/*.fq | head -n 1 )
     ln -sf $file ./set1_00.fq
     ln -sf set1_00.fq set1.fq
+    ln -s set1.fq read_1
     file=$( ls -1 ../$dir/*.fq | tail -n 1 )
     ln -sf $file ./set2_00.fq
     ln -sf set2_00.fq set2.fq
+    ln -s set2.fq read_2
     cd ..
 done
 
